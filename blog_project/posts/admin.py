eff4a8d2d329634e92ab11e4b22d4d7b67f3fca1
Aditya Puranik <mail.adityapuranik@gmail.com>
2025-09-21 18:17:11 +0530

    successfully migrated changes

--- blog_project/posts/admin.py
+++ blog_project/posts/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Post
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at')
+    list_filter = ('created_at', 'author')
+
 admin.site.register(Post)
